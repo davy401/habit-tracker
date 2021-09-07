d0322f5f0539f2d5b3b79ffa57d1fe0335f4564e
Bang <01023891638@naver.com>
2021-09-07 15:04:43 +0900

    add habit tracker

---
+++ src/components/habits.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Habit from './habit'
+import HabitAddForm from './habitAddForm';
+
+class Habits extends Component {
+    componentDidUpdate() {
+        // 굉장히 무거운 일을 해야 한다면
+    };
+
+    handleIncrement = habit => {
+        this.props.onIncrement(habit);
+    };
+    handleDecrement = habit => {
+        this.props.onDecrement(habit);
+    };
+    handleDelete    = habit => {
+        this.props.onDelete(habit);
+    };
+    handleAdd       = name => {
+        this.props.onAdd(name);
+    };
+    
+    render() {
+        console.log('habits');
+        return (
+        <>
+            <HabitAddForm onAdd={this.handleAdd} />
+            <ul>
+                {
+                    this.props.habits.map(habit => (
+                        <Habit 
+                            key={habit.id}
+                            habit={habit}
+                            onIncrement={this.handleIncrement}
+                            onDecrement={this.handleDecrement}
+                            onDelete={this.handleDelete}
+                        />
+                    ))
+                }
+            </ul>
+            <button className="habits-reset" onClick={this.props.onReset}>Reset All</button>
+        </>
+        );
+    }
+}
+
+export default Habits;
